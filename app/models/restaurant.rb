503368cf274502afc9227ecd08b2403233024160
Denise Hermoza <denise.hmm91@gmail.com>
2021-09-29 10:54:14 -0500

    validations in models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
+  LIST = ['chinese', 'italian', 'japanese', 'french', 'belgian'].freeze
   has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
 end
